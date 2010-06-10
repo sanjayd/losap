cb07f93291bed9e94c57524719a877a25ac4dc97
Sanjay Deshmukh <sanjayd@gmail.com>
2010-06-10 09:54:02 -0400

    Seeded the LockedMonths to contain all months before the current one in the current year

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
+# Add unit types
 %w(Engine Truck Ambulance).each do |s|
   UnitType.create(:name => s)
+end
+
+# Lock months in current year prior to current month
+(1 .. Date.today.month - 1).each do |month|
+  LockedMonth.create(:month => Date.parse("#{Date.today.year}-#{Date.today.month}-1"))
 end
