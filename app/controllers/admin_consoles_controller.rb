f0bddefcdc91d22bbd7572da008ca9b23da47c91
Sanjay Deshmukh <sanjayd@gmail.com>
2010-06-12 10:46:08 -0400

    Refactored Lock Month to be consistent with others

--- app/controllers/admin_consoles_controller.rb
+++ app/controllers/admin_consoles_controller.rb
@@ -2,5 +2,6 @@ class AdminConsolesController < ApplicationController
   def show
     @locked_month = LockedMonth.new
     @locked_months = LockedMonth.last_two_years
+    @unlocked_months = LockedMonth.unlocked_in_last_two_years
   end
 end
--- app/models/locked_month.rb
+++ app/models/locked_month.rb
@@ -7,7 +7,23 @@ class LockedMonth < ActiveRecord::Base
       :conditions => ["month >= ?", 2.years.ago.to_date],
       :order => 'month DESC').map {|month| month.month}
   end
-  
+
+  def self.oldest_month
+    [SleepIn.oldest.date, Standby.oldest.date].min.beginning_of_month
+  end
+
+  def self.unlocked_in_last_two_years
+    months = []
+    
+    (1 .. 24).each do |num_months|
+      month = Date.today.beginning_of_month - num_months.months
+      break if self.oldest_month > month
+      months << month
+    end
+    
+    months - self.last_two_years
+  end
+
   def self.locked?(month)
     self.find_by_month(month.beginning_of_month)
   end
