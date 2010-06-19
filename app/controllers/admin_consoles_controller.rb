de3d0ab73e6a2203b0e0adf883b914e156aabc99
Sanjay Deshmukh <sanjayd@gmail.com>
2010-06-19 16:43:25 -0400

    Added very basic User Management functionality

--- app/controllers/admin_consoles_controller.rb
+++ app/controllers/admin_consoles_controller.rb
@@ -4,5 +4,6 @@ class AdminConsolesController < ApplicationController
     @all_months = LockedMonth.last_six_months
     @locked_months = LockedMonth.locked_in_last_six_months
     @unlocked_months = LockedMonth.unlocked_in_last_six_months
+    @members = Member.all(:order => 'lastname ASC, firstname ASC, badgeno ASC')
   end
 end
