e048f5a2bc6823dd825b8be60b22cc8d5ddb9f5b
Sanjay Deshmukh <sanjayd@gmail.com>
2010-06-25 21:24:06 -0400

    Admins can now change passwords

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -17,5 +17,21 @@ class AdminsController < ApplicationController
       end
     end
   end
+
+  def edit
+    @admin = current_admin
+  end
   
+  def update
+    @admin = current_admin
+    
+    respond_to do |wants|
+      if @admin.update_attributes(params[:admin])
+        flash[:notice] = "Password Changed"
+        wants.html { redirect_to(admin_console_path) }
+      else
+        wants.html { render :action => "edit" }
+      end
+    end
+  end
 end
