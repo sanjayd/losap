15590055b0aedded23fefe400bfb9f39bc2fdc4b
Sanjay Deshmukh <sanjayd@gmail.com>
2011-05-14 12:08:18 -0400

    Fixed the n + 1 queries problem in monthly reports

--- app/models/report.rb
+++ app/models/report.rb
@@ -16,11 +16,47 @@ class Report
     @members ||= Member.all
   end
 
+  def sleep_ins
+    @sleep_ins ||= build_hash(SleepIn.by_month(@month))
+  end
+
+  def sleep_in_points(member)
+    traverse_hash(sleep_ins, member, :points)
+  end
+  
+  def standbys
+    @standbys ||= build_hash(Standby.by_month(@month))
+  end
+
+  def standby_points(member)
+    traverse_hash(standbys, member, :points)
+  end
+  
+  def hours(member)
+    traverse_hash(sleep_ins, member, :hours) + traverse_hash(standbys, member, :hours)
+  end
+
   def monthly?
     @type == :monthly
   end
   
   def annual?
     @type == :annual
+  end
+
+  private
+  def build_hash(list)
+    hash = {}
+    list.each do |o|
+      hash[o.member_id] ? hash[o.member_id] << o : hash[o.member_id] = [o]
+    end
+    hash
+  end
+  
+  def traverse_hash(hash, member, method)
+    return 0 unless hash[member.id]
+    hash[member.id].inject(0) do |total, o|
+      total + o.send(method)
+    end
   end
 end
