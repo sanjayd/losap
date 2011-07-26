63922b1ecb0fe4124441ad5bda7678a8f229ea8d
Sanjay Deshmukh <sanjayd@gmail.com>
2011-07-26 18:09:42 -0400

    Changed some scopes to use the new query syntax

--- app/models/sleep_in.rb
+++ app/models/sleep_in.rb
@@ -11,17 +11,18 @@ class SleepIn < ActiveRecord::Base
   belongs_to :member
 
   scope :by_year,
-    lambda { |year| { :conditions => ['date >= ? and date <= ?',
-      year.to_date.beginning_of_year, 
-      year.to_date.end_of_year],
-      :order => "date asc" } }
+    lambda { |year|
+      where('date >= ?', year.to_date.beginning_of_year)\
+      .where('date <= ?', year.to_date.end_of_year)\
+      .order('date asc')
+    }
   
   scope :by_month, 
-    lambda { |month| { :conditions => ['date >= ? and date <= ?', 
-      month.to_date.beginning_of_month,
-      month.to_date.end_of_month],
-      :order => "date asc" } }
-  
+    lambda { |month|
+      where('date >= ?', month.to_date.beginning_of_month)\
+      .where('date <= ?', month.to_date.end_of_month)\
+      .order('date asc')
+    }
 
   def hours
     self.deleted? ? 0 : HOURS
