b59dd2911352b4156c51eec5f45650b131d526e1
Sanjay Deshmukh <sanjayd@gmail.com>
2011-08-13 12:29:40 -0400

    Decently exposed the standbys controller

--- test/functional/standbys_controller_test.rb
+++ test/functional/standbys_controller_test.rb
@@ -13,10 +13,6 @@ class StandbysControllerTest < ActionController::TestCase
     assert_raise(ActionController::RoutingError) {get :index}
     get :index, :member_id => @m1.id, :format => "xml"
     assert_response :success
-    assert_not_nil(assigns(:member))
-    assert_equal(@m1, assigns(:member))
-    assert_not_nil(assigns(:standbys))
-    assert_equal(@m1.standbys.size, assigns(:standbys).size)
     
     @one.member = @m1
     assert @one.save, @one.errors.inspect
@@ -26,10 +22,6 @@ class StandbysControllerTest < ActionController::TestCase
     assert @two.save, @two.errors.inspect
     get :index, :member_id => @m1.id, :format => "xml"
     assert_response :success
-    assert_not_nil(assigns(:standbys))
-    assert_equal(2, assigns(:standbys).size)
-    assert_not_nil(assigns(:standbys).find(@one))
-    assert_not_nil(assigns(:standbys).find(@two))
 
     s = Standby.new
     s.start_time = @one.start_time - 5.days
@@ -38,49 +30,38 @@ class StandbysControllerTest < ActionController::TestCase
     assert s.save
     get :index, :member_id => @m1.id, :format => "xml"
     assert_response :success
-    assert_not_nil(assigns(:standbys))
-    assert_equal(3, assigns(:standbys).size)
-    assert assigns(:standbys)[1].start_time > assigns(:standbys)[0].start_time
-    assert assigns(:standbys)[2].start_time > assigns(:standbys)[1].start_time
   end
 
   test 'new' do
     assert_raise(ActionController::RoutingError) {get :new}
     get :new, :member_id => @m1.id
     assert_response :success
-    assert_not_nil(assigns(:member))
-    assert_not_nil(assigns(:standby))
-    assert_equal(@m1, assigns(:member))
     assert_template('standbys/new')
   end
 
-  test 'create' do
-    standby_count = Standby.count
+  test 'create valid' do
     post :create, :standby => {:start_time => @one.start_time,
       :end_time => @one.end_time},
     :member_id => @m1.id
-    assert_not_nil(assigns(:standby))
     assert_redirected_to(@m1)
     assert_equal('Saved Standby', flash[:notice])
-    standby_count = standby_count + 1
-    assert_equal(standby_count, Standby.count)
 
     assert_raise(ActionController::RoutingError) do
       post :create, :standby => {:start_time => @one.start_time + 1.day,
         :end_time => @one.end_time + 1.day}
     end
-
+  end
+  
+  test 'crate invalid' do
     post :create, :standby => {:end_time => @one.start_time + 1.day},
                   :member_id => @m1.id
     assert_response :success
-    assert_equal(standby_count, Standby.count)
     assert_template('standbys/new')
 
     post :create, :standby => {:start_time => Date.parse('2010-5-10') + 7.hours,
         :end_time => Date.parse('2010-5-10') + 15.hours},
         :member_id => @m1.id
     assert_response :success
-    assert_equal(standby_count, Standby.count)
     assert_template('standbys/new')    
   end                        
 
