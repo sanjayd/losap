9f8a794ff98b273f6d32934a4e07e1bb8b5c376a
Sanjay Deshmukh <sanjayd@gmail.com>
2011-08-13 12:07:11 -0400

    Decently exposed the reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,14 +1,14 @@
 class ReportsController < ApplicationController
   load_and_authorize_resource
+  
+  expose(:report) {Report.new(params[:date])}
 
   def show
-    @report = Report.new(params[:date])
-
     respond_to do |format|
-      if @report.annual?
+      if report.annual?
         format.html {render :layout => "annual_report",
                       :template => "reports/annual/show"}
-      elsif @report.monthly?
+      elsif report.monthly?
         format.js {render :layout => false, 
                       :template => "reports/monthly/show" }
       end
