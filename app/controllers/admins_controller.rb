35a97ae17fff77dd14e727d9a731481686b78173
Sanjay Deshmukh <sanjayd@gmail.com>
2011-08-13 11:10:19 -0400

    Decently exposed the admins controller

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,52 +1,47 @@
 class AdminsController < ApplicationController
   load_and_authorize_resource
+
+  expose(:admin)
   
   def new
-    @admin = Admin.new
   end
   
   def create
-    @admin = Admin.new(params[:admin])
-    
-    respond_to do |wants|
-      if @admin.save
+    respond_to do |format|
+      if admin.save
         flash[:notice] = 'Registered New Admin'
-        wants.html { redirect_to(admin_console_path) }
-        wants.xml { render :xml => @admin, :status => :created, :location => @admin }
+        format.html { redirect_to(admin_console_path) }
+        format.xml { render :xml => admin, :status => :created, :location => admin }
       else
-        wants.html { render :action => "new" }
-        wants.xml { render :xml => @admin.errors, :status => :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml { render :xml => admin.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def edit
-    @admin = Admin.find(params[:id])
   end
   
   def update
-    @admin = Admin.find(params[:id])
-    
-    respond_to do |wants|
+    respond_to do |format|
       if !current_admin.valid_password? params[:old_password]
         flash[:warning] = 'Current password is incorrect'
-        wants.html { render :action => "edit" }
-      elsif @admin.update_attributes(params[:admin])
+        format.html { render :action => "edit" }
+      elsif admin.update_attributes(params[:admin])
         flash[:notice] = "Password Changed"
-        wants.html { redirect_to(admin_console_path) }
+        format.html { redirect_to(admin_console_path) }
       else
-        wants.html { render :action => "edit" }
+        format.html { render :action => "edit" }
       end
     end
   end
   
   def destroy
-    @admin = Admin.find(params[:id])
-    @admin.destroy
+    admin.destroy
     
-    respond_to do |wants|
+    respond_to do |format|
       flash[:notice] = "Admin deleted"
-      wants.html { redirect_to(admin_console_path) }
+      format.html { redirect_to(admin_console_path) }
     end
   end
 end
